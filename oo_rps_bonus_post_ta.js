65cb546b345364776a0d2027c43789986d02016f
lxnow <laurence@tasker.ph>
2022-03-01 09:12:31 +0800

    updated post TA RPS

--- oo_rps_bonus_post_ta.js
+++ oo_rps_bonus_post_ta.js
@@ -1,106 +1,5 @@
 const readline = require('readline-sync');
 
-const createHuman = () => {
-  let playerObject = createPlayer('human');
-
-  let humanObject = {
-
-    shortcutChoices: {
-      1: 'rock',
-      2: 'paper',
-      3: 'scissors',
-      4: 'spock',
-      5: 'lizard',
-    },
-
-    choose() {
-      let choice;
-
-      while (true) {
-        console.log('Please choose \x1b[96m1\x1b[0m-rock, \x1b[96m2\x1b[0m-paper, \x1b[96m3\x1b[0m-scissors, \x1b[96m4\x1b[0m-spock, \x1b[96m5\x1b[0m-lizard or \x1b[96mh\x1b[0mistory of moves:');
-        choice = readline.question();
-        if (['rock', 'paper', 'scissors', 'lizard', 'spock', '1', '2', '3', '4', '5'].includes(choice.toLowerCase())) break;
-        else if (choice === 'h' || choice === 'history') RPSGame.displayHistory();
-        else console.log('Sorry, invalid choice');
-      }
-      if (this.shortcutChoices[choice]) {
-        this.move = this.shortcutChoices[choice];
-      } else this.move = choice;
-      this.logMove(this);
-      RPSGame.computer.rebaseProbabilities(this.move);
-    },
-  };
-  return Object.assign(playerObject, humanObject);
-};
-
-const createComputer = () => {
-  let playerObject = createPlayer('computer');
-
-  let computerObject = {
-    probBase: {
-      rock: 20,
-      paper: 20,
-      scissors: 20,
-      spock: 20,
-      lizard: 20,
-    },
-
-    probTiers: {
-      rock: 20,
-      paper: 20,
-      scissors: 20,
-      spock: 20,
-      lizard: 20,
-    },
-
-    PROB_ADD: 10,
-
-    choose() {
-      let choice = Math.random();
-      this.move = this.smarterChoice(choice);
-      this.logMove(this);
-    },
-
-    rebaseProbabilities(choice) {
-      this.increaseWeights(choice);
-      let sum = Object.values(this.probBase).reduce((a, b) => a + b);
-      let prevSum = 0;
-      for (let move in this.probTiers) {
-        this.probTiers[move] = (this.probBase[move] / sum) + prevSum;
-        prevSum = this.probTiers[move];
-      }
-    },
-
-    increaseWeights(choice) {
-      let loseFromOther = {
-        rock: ['paper', 'spock'],
-        paper: ['lizard', 'scissors'],
-        scissors: ['rock', 'spock'],
-        spock: ['lizard', 'paper'],
-        lizard: ['rock', 'scissors']
-      };
-      loseFromOther[choice].forEach(element => {
-        this.probBase[element] += this.PROB_ADD;
-      });
-    },
-
-    smarterChoice(choiceNumber) {
-      let finalChoice;
-      let prevMoveValue = 0;
-      let move;
-      for (move in this.probTiers) {
-        if (choiceNumber > prevMoveValue
-          && choiceNumber <= this.probTiers[move]) {
-          finalChoice = move;
-        }
-        prevMoveValue = this.probTiers[move];
-      }
-      return finalChoice;
-    },
-  };
-  return Object.assign(playerObject, computerObject);
-};
-
 const RPSGame = {
   human: createHuman(),
   computer: createComputer(),
@@ -199,6 +98,107 @@ function createPlayer(name) {
     },
 
   };
+}
+
+function createHuman () {
+  let playerObject = createPlayer('human');
+
+  let humanObject = {
+
+    shortcutChoices: {
+      1: 'rock',
+      2: 'paper',
+      3: 'scissors',
+      4: 'spock',
+      5: 'lizard',
+    },
+
+    choose() {
+      let choice;
+
+      while (true) {
+        console.log('Please choose \x1b[96m1\x1b[0m-rock, \x1b[96m2\x1b[0m-paper, \x1b[96m3\x1b[0m-scissors, \x1b[96m4\x1b[0m-spock, \x1b[96m5\x1b[0m-lizard or \x1b[96mh\x1b[0mistory of moves:');
+        choice = readline.question();
+        if (['rock', 'paper', 'scissors', 'lizard', 'spock', '1', '2', '3', '4', '5'].includes(choice.toLowerCase())) break;
+        else if (choice === 'h' || choice === 'history') RPSGame.displayHistory();
+        else console.log('Sorry, invalid choice');
+      }
+      if (this.shortcutChoices[choice]) {
+        this.move = this.shortcutChoices[choice];
+      } else this.move = choice;
+      this.logMove(this);
+      RPSGame.computer.rebaseProbabilities(this.move);
+    },
+  };
+  return Object.assign(playerObject, humanObject);
+}
+
+function createComputer () {
+  let playerObject = createPlayer('computer');
+
+  let computerObject = {
+    probBase: {
+      rock: 20,
+      paper: 20,
+      scissors: 20,
+      spock: 20,
+      lizard: 20,
+    },
+
+    probTiers: {
+      rock: 20,
+      paper: 20,
+      scissors: 20,
+      spock: 20,
+      lizard: 20,
+    },
+
+    PROB_ADD: 10,
+
+    choose() {
+      let choice = Math.random();
+      this.move = this.smarterChoice(choice);
+      this.logMove(this);
+    },
+
+    rebaseProbabilities(choice) {
+      this.increaseWeights(choice);
+      let sum = Object.values(this.probBase).reduce((a, b) => a + b);
+      let prevSum = 0;
+      for (let move in this.probTiers) {
+        this.probTiers[move] = (this.probBase[move] / sum) + prevSum;
+        prevSum = this.probTiers[move];
+      }
+    },
+
+    increaseWeights(choice) {
+      let loseFromOther = {
+        rock: ['paper', 'spock'],
+        paper: ['lizard', 'scissors'],
+        scissors: ['rock', 'spock'],
+        spock: ['lizard', 'paper'],
+        lizard: ['rock', 'scissors']
+      };
+      loseFromOther[choice].forEach(element => {
+        this.probBase[element] += this.PROB_ADD;
+      });
+    },
+
+    smarterChoice(choiceNumber) {
+      let finalChoice;
+      let prevMoveValue = 0;
+      let move;
+      for (move in this.probTiers) {
+        if (choiceNumber > prevMoveValue
+          && choiceNumber <= this.probTiers[move]) {
+          finalChoice = move;
+        }
+        prevMoveValue = this.probTiers[move];
+      }
+      return finalChoice;
+    },
+  };
+  return Object.assign(playerObject, computerObject);
 }
 
 RPSGame.play();
